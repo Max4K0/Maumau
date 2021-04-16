4ef50564362da4fbc3553d2fdbb634fdd6730ca9
Max <max08122000@gmail.com>
2021-04-16 10:55:19 +0200

    Version 0.1

--- src/main/Main.scala
+++ src/main/Main.scala
@@ -6,9 +6,10 @@ object Main {
     val arraystring = List(1, 2, 3, 4, 5, 6, 7, 8, 9)
     val newarraystring = for(array <- arraystring if array < 5)yield array
 
-    println("Hello World")
+    println("Hello World");
     println(arraystring)
     println(newarraystring)
+
 
   }
 
