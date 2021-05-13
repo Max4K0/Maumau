b538e3101fa2ebb36ac8268827904228b1e4cbdd
Daniel Schellinger <d.schellinger1@gmail.com>
2021-05-13 21:49:50 +0200

    Version 0.3     -state fix

--- src/main/scala/de/htwg/se/maumau/util/GameState.scala
+++ src/main/scala/de/htwg/se/maumau/util/GameState.scala
@@ -3,25 +3,25 @@ package de.htwg.se.maumau.util
 trait GameState
 
 case class invalidThrowEvent() extends GameState {
-  def invalidThrow = println("you cant throw this card")
+
 }
 
 case class nextPlayerEvent() extends GameState {
-  def nextPlayer = println("commands: throw card, take card, q for Quit")
+
 }
 
 case class invalidPullEvent() extends GameState {
-  def invalidPull = println("you cant pull a card, cause you can throw one")
+
 }
 
 case class unknownCommandEvent() extends GameState {
-  def unknownCommand = println("invalid command")
+
 }
 
 case class winEvent() extends GameState {
-  def gameOver = println("the current player won!")
+
 }
 
 case class startEvent() extends GameState {
-  def start = println("welcome!")
+
 }
--- src/main/scala/de/htwg/se/maumau/util/State.scala
+++ src/main/scala/de/htwg/se/maumau/util/State.scala
@@ -4,13 +4,19 @@ object State {
   var state = println("")
   def handle(gs: GameState) = {
     gs match {
-      case a: invalidThrowEvent => state = invalidThrowEvent().invalidThrow
-      case b: nextPlayerEvent => state = nextPlayerEvent().nextPlayer
-      case c: invalidPullEvent => state = invalidPullEvent().invalidPull
-      case d: unknownCommandEvent => state = unknownCommandEvent().unknownCommand
-      case e: winEvent => state = winEvent().gameOver
-      case f: startEvent => state = startEvent().start
+      case a: invalidThrowEvent => state = invalidThrowEvent
+      case b: nextPlayerEvent => state = nextPlayerEvent
+      case c: invalidPullEvent => state = invalidPullEvent
+      case d: unknownCommandEvent => state = unknownCommandEvent
+      case e: winEvent => state = winEvent
+      case f: startEvent => state = startEvent
     }
     state
   }
+  def invalidThrow = println("you cant throw this card")
+  def nextPlayer = println("commands: throw card, take card, q for Quit")
+  def invalidPull = println("you cant pull a card, cause you can throw one")
+  def unknownCommand = println("invalid command")
+  def gameOver = println("the current player won!")
+  def start = println("welcome!")
 }
