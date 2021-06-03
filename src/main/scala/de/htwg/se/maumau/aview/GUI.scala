de552542a12890bf280748136ba041cb2268bee8
Daniel Schellinger <d.schellinger1@gmail.com>
2021-06-03 19:04:17 +0200

    Version 0.5     - ImageButton Vers2

--- src/main/scala/de/htwg/se/maumau/aview/GUI.scala
+++ src/main/scala/de/htwg/se/maumau/aview/GUI.scala
@@ -2,15 +2,16 @@ package de.htwg.se.maumau.aview
 
 import de.htwg.se.maumau.controller.Controller
 import de.htwg.se.maumau.model.Card
+import scalafx.Includes.observableList2ObservableBuffer
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
-import scalafx.geometry.{BoundingBox, Insets}
+import scalafx.geometry.{BoundingBox, Insets, Pos}
 import scalafx.scene.control.Button
 import scalafx.scene.{Scene, SubScene}
 import scalafx.scene.effect.{DropShadow, Light, Lighting}
 import scalafx.scene.image._
 import scalafx.scene.input.MouseEvent
-import scalafx.scene.layout.{FlowPane, GridPane, HBox}
+import scalafx.scene.layout.{FlowPane, GridPane, HBox, VBox}
 import scalafx.scene.paint.Color._
 import scalafx.scene.paint.{Color, LinearGradient, Stops}
 import scalafx.scene.text.Text
@@ -29,22 +30,64 @@ case class GUI (guiApp: GUIApp, controller: Controller) extends JFXApp {
        val cardSizeY = 726 * cardRatio
        val img = new Image(controller.table.player(1).playerDeck.cards.head.imgPath, cardSizeX, cardSizeY, true, false)
        val view = new ImageView(img)
-       content = new HBox {
+       content += new HBox {
 
-         padding = Insets(100, 200, 220, 220)
+         padding = Insets(100, 100, 700, 700)
          children = Seq(
+
            new Button() {
            this.setGraphic(view)
            this.setStyle("-fx-background-color: transparent")
-           effect = new Lighting {
+           effect = new Lighting
+           this.visible = true
+           this.setLayoutX(0)
+         },
 
-
-           }
-           content = view
+         new Button() {
+           this.setGraphic(new ImageView(new Image(controller.table.player(1).playerDeck.cards.last.imgPath, cardSizeX, cardSizeY, true, false)))
+           this.setStyle("-fx-background-color: transparent")
+           effect = new Lighting
+           this.visible = true
            this.setLayoutX(0)
          }
          )
        }
+       content += new HBox {
+         padding = Insets(500, 0, 0, 0)
+         children = Seq(
+
+           new Button() {
+             this.setGraphic(new ImageView(new Image(controller.table.player(0).playerDeck.cards.head.imgPath, cardSizeX, cardSizeY, true, false)))
+             this.setStyle("-fx-background-color: transparent")
+             effect = new Lighting
+             this.visible = true
+             //this.setLayoutX(0)
+           },
+
+           new Button() {
+             this.setGraphic(new ImageView(new Image(controller.table.player(0).playerDeck.cards.last.imgPath, cardSizeX, cardSizeY, true, false)))
+             this.setStyle("-fx-background-color: transparent")
+             effect = new Lighting
+             this.visible = true
+             //this.setLayoutX(0)
+           }
+
+         )
+       }
+       for (x <- 0 to 1)
+         content += new HBox {
+           padding = Insets(500 * x , 0, 0, 0)
+             children = Seq(
+               new Button() {
+                 this.setGraphic(new ImageView(new Image(controller.table.player(0).playerDeck.cards.head.imgPath, cardSizeX, cardSizeY, true, false)))
+                 this.setStyle("-fx-background-color: transparent")
+                 effect = new Lighting
+                 this.visible = true
+
+               }
+
+           )
+         }
      }
    }
 
