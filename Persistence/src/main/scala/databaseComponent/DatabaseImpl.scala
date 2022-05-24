9a3e3a4ce0a0a6f832d03c35fb97def2bfc39b9d
Tom Kosacki <tom.kosacki@gmail.com>
2022-05-25 01:20:15 +0200

    Version 1.6 - slick continue

--- Persistence/src/main/scala/databaseComponent/DatabaseImpl.scala
+++ Persistence/src/main/scala/databaseComponent/DatabaseImpl.scala
@@ -2,13 +2,12 @@ package databaseComponent
 
 import com.google.inject.Inject
 import databaseComponent.DatabaseInterface
-
 import slick.jdbc.JdbcBackend.Database
 import slick.lifted.TableQuery
 import slick.jdbc.PostgresProfile.api.*
-
 import play.api.libs.json.{JsArray, JsValue, Json}
 
+import scala.collection.mutable.ListBuffer
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.{Duration, DurationInt}
 import scala.concurrent.{Await, Future}
@@ -31,29 +30,41 @@ class DatabaseImpl @Inject () extends DatabaseInterface {
       driver = "org.postgresql.Driver"
     )
 
-
-  def writeCard(card: String): Int
-  def readCard(id: Int): String
-
-  def writeCardList(cards: List[String]): List[Int]
+  def writeCardList(deckId: Int, cards: List[String]): List[Int] = {
+    for (card <- cards){
+      val query = sqlu"""INSERT INTO CARDS VALUES ($deckId, $card)"""
+      Await.result(database.run(query), atMost = 10.second)
+    }
+  }
   def readCardList(id: Int): List[String]
 
-  def writeDeck(deck: List[String]): List[Int]
+  def writeDeck(deck: List[String]): Int = {
+    val deckIdQuery = sql"""SELECT MAX(id) FROM DECKS""".as[Int]
+    val deckId : Int = Await.result[Int](database.run(deckIdQuery), atMost = 10.second)
+    val cardListId : Int = writeCardList(deckId, deck)
+    val query = sqlu"""INSERT INTO DECKS VALUES ($deckId)"""
+  }
   def readDeck(id: Int): List[String]
 
   def writeDeckList(decks: List[String]): List[Int]
   def readDeckList(id: Int): List[List[String]]
 
-  def writePlayer(player: String): Int
+  def writePlayer(name: String, deck: String): Int = {
+    val deckId : Int = writeDeck(Json.parse(deck).as[List[String]])
+    val query = sqlu"""INSERT INTO PLAYERS VALUES ($name, $deckId);"""
+    Await.result[Int](database.run(query), atMost = 10.second)
+  }
   def readPlayer(id: Int): String
 
   def writePlayerList(players: List[String]): List[Int] = {
+    val ids = ListBuffer[Int]()
     for (player <- players) {
       val playerJson: JsValue = Json.parse(player)
       val playerName: String = (playerJson \ "name").get.toString
-      val playerDeck: List[String] = (playerJson \ "playerDeck").get.as[List[String]]
-      
+      val playerDeck: String = (playerJson \ "playerDeck").get.as[String]
+      ids += writePlayer(playerName, playerDeck)
     }
+    ids.toList
   }
   def readPlayerList(id: Int): List[String]
 
@@ -62,7 +73,7 @@ class DatabaseImpl @Inject () extends DatabaseInterface {
     val playerIds: List[Int] = writePlayerList((tableJson \ "player").get.as[List[String]])
     val deckIds: List[Int] = writeDeckList((tableJson \ "tableDecks").get.as[List[String]])
 
-    val query = sql"""INSERT INTO GAME_TABLES VALUES ('${playerIds.head}', '${playerIds.last}', '${deckIds.head}', '${deckIds.last}');""".as[(Int)]
+    val query = sqlu"""INSERT INTO GAME_TABLES VALUES ('${playerIds.head}', '${playerIds.last}', '${deckIds.head}', '${deckIds.last}');"""
     Await.result(database.run(query), Duration.Inf)
   }
   def readTable(): String
--- Persistence/src/main/scala/databaseComponent/DatabaseInterface.scala
+++ Persistence/src/main/scala/databaseComponent/DatabaseInterface.scala
@@ -1,15 +1,13 @@
 package databaseComponent
 
 trait DatabaseInterface {
-  def writeCard(card: String): Int
-  def readCard(id: Int): String
-  def writeCardList(cards: List[String]): List[Int]
+  def writeCardList(deckId: Int, cards: List[String]): List[Int]
   def readCardList(id: Int): List[String]
-  def writeDeck(deck: List[String]): List[Int]
+  def writeDeck(deck: List[String]): Int
   def readDeck(id: Int): List[String]
   def writeDeckList(decks: List[List[String]]): List[Int]
   def readDeckList(id: Int): List[List[String]]
-  def writePlayer(player: String): Int
+  def writePlayer(name: String, deck: String): Int
   def readPlayer(id: Int): String
   def writePlayerList(players: List[String]): List[Int]
   def readPlayerList(id: Int): List[String]
